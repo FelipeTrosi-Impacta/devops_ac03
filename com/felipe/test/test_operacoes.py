7d3e7f33af3b621f1b210f6ec4300353a5031a00
FelipeTrosi-Impacta <69995013+FelipeTrosi-Impacta@users.noreply.github.com>
2020-10-08 22:25:31 -0300

    Update test_operacoes.py

--- com/felipe/test/test_operacoes.py
+++ com/felipe/test/test_operacoes.py
@@ -1,6 +1,11 @@
-import pytest
-from com.felipe.operacoes import Operacoes
+def func(x):
+    return x + 1
 
-def test_soma():
-    operacoes = Operacoes()
-    assert operacoes.func(1) == 2, "Should be 2"
+def test_positivo():
+    assert func(3) == 4
+
+def test_negativo():
+    assert func(-2) == -1
+      
+def test_zero():
+    assert func(0) == 1
