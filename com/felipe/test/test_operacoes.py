ac69e7d409b817d8c48f06b4134e4e352ced7b54
FelipeTrosi-Impacta <69995013+FelipeTrosi-Impacta@users.noreply.github.com>
2020-10-08 22:26:49 -0300

    Update test_operacoes.py

--- com/felipe/test/test_operacoes.py
+++ com/felipe/test/test_operacoes.py
@@ -1,11 +1,5 @@
 def func(x):
     return x + 1
 
-def test_positivo():
+def test_soma():
     assert func(3) == 4
-
-def test_negativo():
-    assert func(-2) == -1
-      
-def test_zero():
-    assert func(0) == 1
